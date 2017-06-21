1771c0dab85c5253025b93707fda838faa94265e
SanthiyaK <ksanthiya93@gmail.com>
2017-06-21 15:29:59 +0530

    Create v.c

---
+++ v.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+int ch;
+scanf("%d",&ch);
+if(ch==a||ch==e||ch==i||ch==o||ch==u)
+printf("entered alphabet is vowel");
+else
+printf("entered alphabet is consonents");
+}
