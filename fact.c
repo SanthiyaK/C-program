48f25d90bfdc952eee601899ca3d5635b31f75e5
SanthiyaK <ksanthiya93@gmail.com>
2017-07-13 17:36:48 +0530

    Update fact.c

--- fact.c
+++ fact.c
@@ -2,10 +2,11 @@
 void main()
 {
 int fact=1,i,n;
+printf("enter the factorial number");
 scanf("%d",&n;
 for(i=0;i<n;i++)
 {
 fact=fact*i;
 }
-printf("%d",fact);
+printf("the factorial no is %d",fact);
 }
