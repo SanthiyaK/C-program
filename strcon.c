450012f29df4c8a967f4d3604d481daa47b3b55b
SanthiyaK <ksanthiya93@gmail.com>
2017-08-31 14:20:39 +0530

    Create strcon.c

---
+++ strcon.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int i,a[10],b[10];
+for(i=0;a[i]!='\0';i++)
+{
+  c[i]=a[i];
+  i++;
+}
+for(j=0;b[j]!='\0';j++)
+{
+   c[i]=b[i];
+   i++;
+}
+c[i]='\0';
+printf(" The Concatenetion of two numbers are %s",c);
+}
